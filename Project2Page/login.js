21cd2c584158905ae3f0366c87786cf379d08470
CGORDONSEC <cgordonsec@gmail.com>
2024-08-12 14:56:47 -0500

    Removing Project2Page

--- Project2Page/login.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("user").onclick=function() {userLogin()};
-
-function userLogin(){
-    const name ={userName: document.getElementById("login").value};
-    /*document.getElementById("user").onclick=*/
-}
